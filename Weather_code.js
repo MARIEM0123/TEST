ea79763fe83716c5f6f3fa9cae8b13d56eecb743
MARIEM0123 <129907944+MARIEM0123@users.noreply.github.com>
2024-07-09 15:23:58 +0100

    Update Weather_code.js

--- Weather_code.js
+++ Weather_code.js
@@ -38,7 +38,7 @@ function getWeatherData () {
         
         let {latitude, longitude } = success.coords;
 
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
 
         console.log(data)
         showWeatherData(data);
